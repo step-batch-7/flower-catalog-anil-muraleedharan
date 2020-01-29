ed726f8bc3a172dccaaa60fa0a27156635e5f07a
Anil Muraleedharan <56071561+anil-muraleedharan@users.noreply.github.com>
2020-01-29 18:55:24 +0530

    Change the server from net module server to http module based

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const fs = require('fs');
-const Response = require('./lib/response');
-const CONTENT_TYPES = require('./lib/mimeTypes');
-
+const querystring = require('querystring');
+const STATIC_FOLDER = `${__dirname}/public`;
+const TEMPLATES_FOLDER = `${__dirname}/templates`;
+const COMMENTS_FILE_PATH = `${__dirname}/data/comments.json`;
+const CONTENT_TYPES = {
+  txt: 'text/plain',
+  html: 'text/html',
+  css: 'text/css',
+  js: 'application/javascript',
+  json: 'application/json',
+  gif: 'image/gif',
+  jpg: 'image/jpg'
+};
 const formats = {
   person: '&#128100;',
   message: '&#128233;',
@@ -9,81 +19,74 @@ const formats = {
     '<br>&ThickSpace;&ThickSpace;&ThickSpace;&ThickSpace;&ThickSpace;&ThinSpace;'
 };
 
-const STATIC_FOLDER = `${__dirname}/public`;
-const commentDatabase = `${__dirname}/data/comments.json`;
+const getPreviousComments = COMMENTS_FILE_PATH =>
+  fs.existsSync(COMMENTS_FILE_PATH) ? require(COMMENTS_FILE_PATH) : [];
 
-const serveStaticFile = (req, path) => {
-  if (!path) path = `${STATIC_FOLDER}${req.url}`;
-  const stat = fs.existsSync(path) && fs.statSync(path);
-  if (!stat || !stat.isFile()) return new Response();
-  const [, extension] = path.match(/.*\.(.*)$/) || [];
-  const contentType = CONTENT_TYPES[extension];
-  const content = fs.readFileSync(path);
-  const res = new Response();
-  res.setHeader('Content-Type', contentType);
-  res.setHeader('Content-Length', content.length);
-  res.statusCode = 200;
-  res.body = content;
-  return res;
+const updateComments = ({ name, comment }) => {
+  let previousComments = getPreviousComments(COMMENTS_FILE_PATH);
+  const timeStamp = new Date().getTime();
+  previousComments.push({ name, comment, timeStamp });
+  fs.writeFileSync(COMMENTS_FILE_PATH, JSON.stringify(previousComments));
 };
 
-const serveHomePage = req => {
-  req.url = '/index.html';
-  return serveStaticFile(req);
-};
-
-const getPreviousComments = () =>
-  fs.existsSync('./data/comments.json')
-    ? JSON.parse(fs.readFileSync('./data/comments.json', 'utf8'))
-    : [];
-
-const commentSetFormatter = ({ name, comment }) => {
+const commentSetFormatter = function({ name, comment, timeStamp }) {
   comment = comment.replace(/\r\n/g, `${formats.newline}`);
-  comment = comment.replace(/ /g, '&ensp;');
+  name = name.replace(/ /g, ' &nbsp');
+  comment = comment.replace(/ /g, '&nbsp;');
+  date = new Date(timeStamp).toUTCString();
   return `
-  <Strong>${formats.person}${name}</Strong><br/>
+  <Strong>${formats.person}${name}</Strong> @ ${date}<br/>
   ${formats.message}
   ${comment}`;
 };
 
-const serveGuestBook = req => {
-  const res = serveStaticFile(req, `${__dirname}/templates/guestBook.html`);
-  let content = res.body.toString();
-  let comments = getPreviousComments().reverse();
-  const formattedComments = comments.map(commentSetFormatter);
+const getGuestPage = function(url) {
+  let comments = getPreviousComments(COMMENTS_FILE_PATH);
+  let guestPageHtml = fs.readFileSync(`${TEMPLATES_FOLDER}/${url}`, 'utf8');
+  const formattedComments = comments.reverse().map(commentSetFormatter);
   const commentsLog = formattedComments.join('<br/><br/>');
-  content = content.replace(/__comments__/, commentsLog);
-  res.body = content;
-  res.setHeader('Content-Length', content.length);
-  return res;
+  guestPageHtml = guestPageHtml.replace(`__comments__`, commentsLog);
+  return guestPageHtml;
 };
 
-const handleComment = req => {
-  let comments = getPreviousComments();
-  let { name, comment } = req.body;
-  comments.push({
-    name,
-    comment
+const serveGuestPage = function(req, res) {
+  let guestPageHtml = getGuestPage(req.url);
+  res.setHeader('Content-Type', CONTENT_TYPES.html);
+  if (req.method === 'GET') return res.end(guestPageHtml);
+  let data = '';
+  req.on('data', text => (data += text));
+  req.on('end', () => {
+    if (req.headers['content-type'] === 'application/x-www-form-urlencoded') {
+      let { name, comment } = querystring.parse(data);
+      let comments = { name, comment };
+      updateComments(comments);
+    }
+    guestPageHtml = getGuestPage(req.url);
+    res.setHeader('location', 'guestBook.html');
+    res.statusCode = 303;
+    res.end(guestPageHtml);
   });
-  fs.writeFileSync(commentDatabase, JSON.stringify(comments));
-  const res = serveGuestBook(req);
-  res.statusCode = 303;
-  res.setHeader('location', './guestBook.html');
-  return res;
 };
 
-const findHandler = req => {
-  if (req.method === 'GET' && req.url === '/') return serveHomePage;
-  if (req.method === 'GET' && req.url === '/guestBook.html')
-    return serveGuestBook;
-  if (req.method === 'GET') return serveStaticFile;
-  if (req.method === 'POST') return handleComment;
-  return () => new Response();
+const serveStaticFile = (req, res) => {
+  const path = `${STATIC_FOLDER}${req.url}`;
+  const stat = fs.existsSync(path) && fs.statSync(path);
+  if (!stat || !stat.isFile()) return res.end('Not Found');
+  const [, extension] = path.match(/.*\.(.*)$/) || [];
+  res.setHeader('Content-Type', CONTENT_TYPES[extension]);
+  res.end(fs.readFileSync(path));
 };
 
-const processRequest = req => {
-  const handler = findHandler(req);
-  return handler(req);
+const serveHomePage = (req, res) => {
+  req.url = '/index.html';
+  serveStaticFile(req, res);
+};
+
+const processRequest = (req, res) => {
+  if (req.method === 'GET' && req.url === '/') return serveHomePage(req, res);
+  if (req.url === '/guestBook.html') return serveGuestPage(req, res);
+  if (req.method === 'GET') return serveStaticFile(req, res);
+  return ((req, res) => res.end('Not Found'))(req, res);
 };
 
 module.exports = { processRequest };
--- server.js
+++ server.js
@@ -1,39 +1,25 @@
-const { Server } = require('net');
-const Request = require('./lib/request');
-const { processRequest } = require('./app');
+const networkInterfaces = require('os').networkInterfaces();
+const http = require('http');
 const fs = require('fs');
+const { processRequest } = require('./app');
 
-const handleConnection = function(socket) {
-  const remote = `${socket.remoteAddress}:${socket.remotePort}`;
-  console.warn('new connection', remote);
-  socket.setEncoding('utf8');
-  socket.on('close', hadError =>
-    console.warn(`${remote} closed ${hadError ? 'with error' : ''}`)
-  );
-  socket.on('end', () => console.warn(`${remote} ended`));
-  socket.on('error', err => console.error('socket error', err));
-  socket.on('drain', () => console.warn(`${remote} drained`));
-  socket.on('data', text => {
-    console.warn(`${remote} data:\n`);
-    const req = Request.parse(text);
-    const res = processRequest(req);
-    res.writeTo(socket);
-  });
+const setUpDataDir = function(dataDirPath) {
+  if (!fs.existsSync(`${dataDirPath}`)) fs.mkdirSync(`${dataDirPath}`);
 };
 
-const setUp = () => {
+const main = function(port = 3333) {
   const dataDirPath = `${__dirname}/data`;
-  if (!fs.existsSync(dataDirPath)) fs.mkdirSync(dataDirPath);
-};
-
-const main = (port = 4000) => {
-  setUp();
-  const server = new Server();
-  server.on('error', err => console.error('server error', err));
-  server.on('connection', handleConnection);
-  server.on('listening', () =>
-    console.warn('started listening', server.address())
+  setUpDataDir(dataDirPath);
+  const serverIp = networkInterfaces['en0'][1].address;
+  const server = new http.Server(processRequest);
+  server.listen(port, () =>
+    console.warn(
+      [
+        'Serving HTTP on',
+        `${serverIp} port ${port}`,
+        `(http://${serverIp}:${port}/)`
+      ].join(' ')
+    )
   );
-  server.listen(port);
 };
 main(process.argv[2]);
