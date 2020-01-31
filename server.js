8fc07648595bbaf7af4806dadce186f90e7a8fff
Anil Muraleedharan <56071561+anil-muraleedharan@users.noreply.github.com>
2020-02-01 00:05:24 +0530

    Pulled out the logging in the server screen when the server starts to  a new logger file

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
-const networkInterfaces = require('os').networkInterfaces();
 const http = require('http');
-const { stdout } = require('process');
 const { app } = require('./lib/handler');
+const { showBeginMessage } = require('./lib/logger');
 
 const defaultPort = 3333;
 
 const main = function(port = defaultPort) {
-  const myIpPosition = 1;
-  const serverIp = networkInterfaces['en0'][myIpPosition].address;
   const server = new http.Server(app.serve.bind(app));
-  server.listen(port, () =>
-    stdout.write(
-      [
-        'Serving HTTP on',
-        `${serverIp} port ${port}`,
-        `(http://${serverIp}:${port}/)`
-      ].join(' ')
-    )
-  );
+  server.listen(port, showBeginMessage.bind(null, port));
 };
+
 const portNumPosition = 2;
 main(process.argv[portNumPosition]);
